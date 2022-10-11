942f75de83cc9a14fcc7ecfa5b17c0b607b11090
Junior Gabi <juniorgabi@juniors-air.home>
2022-10-11 07:44:08 +0100

    creating my cards and responsive site

---
+++ Components/ApprovalCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const LocationCard = (props) =>{
+    return(
+        <div className="BackgroundCard">
+            <h1>Hello {props.name}</h1>
+            <p className="WelcomeMessage">Welcome to our site, We see that you are located
+            {props.location}
+            </p>
+        </div>
+    )
+}
+
+export default LocationCard;
