08f691e7ed6ccada5172a446def39d29c454a8e2
Junior Gabi <juniorgabi@juniors-air.home>
2022-10-11 07:55:49 +0100

    first test

--- Components/ApprovalCard.js
+++ Components/ApprovalCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const LocationCard = (props) =>{
     return(
-        <div className="BackgroundCard">
+        <div className="ui container comments">
             <h1>Hello {props.name}</h1>
             <p className="WelcomeMessage">Welcome to our site, We see that you are located
             {props.location}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ApprovalCard from '../Components/ApprovalCard'
 
 class App extends React.Component{
     constructor(props){
@@ -19,10 +20,17 @@ class App extends React.Component{
     render(){
         if(this.state.lat && !this.state.errorMessage){
             return(
-                <div>Latitude:{this.state.lat}</div>)
+                <ApprovalCard name="Controllah" location={this.state.lat} >
+                <div>Latitude:{this.state.lat}</div>
+                </ApprovalCard>)
         }else if(!this.state.lat && this.state.errorMessage){
             return <div> Error:{this.state.errorMessage} </div>;
             
+        }else{
+            return(
+                <ApprovalCard name="Controllah" location={this.state.lat} >
+                <div>Waiting PLease Be Patient</div>
+                </ApprovalCard>)
         }
     }
 }
