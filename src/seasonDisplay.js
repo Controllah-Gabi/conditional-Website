d63de3114c4138769e641197e3b6a9d7e1ef5f3d
Junior Gabi <juniorgabi@juniors-air.home>
2022-10-12 16:47:41 +0100

    Finished lat video homepage

--- src/seasonDisplay.js
+++ src/seasonDisplay.js
@@ -3,7 +3,7 @@ import video1 from "./pexels-koolshooters-8533112.mp4";
 import video2 from "./pexels-ron-lach-7354255.mp4"
 
 const getSeason = (lat) =>{
-    if (lat === 53.4902162){
+    if (lat < 53.49013327966139){
         return "true";
     }
       else{
@@ -14,7 +14,7 @@ const getSeason = (lat) =>{
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat);
-    const seasonText = season === "true" ? video1:video2;
+    const seasonText = season === "true" ? video2 : video1;
     return(<div className="row"> 
         <video autoPlay loop className="background-vid" width="100%" >
             <source src ={seasonText} type = "video/mp4"/>
